69de3965765059a8a5128bf33e3b4aaded7c3da2
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2024-07-17 09:42:05 -0300

    feat: support untitled songs

--- cutmp3.js
+++ cutmp3.js
@@ -2,28 +2,29 @@ const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const moment = require('moment');
 
-const sourceAudioPath = '/home/jorge/Personal/cutmp3/20230502.mp3';
+const targetSongNamesPrefix = '20240716';
+const sourceAudioPath = `/home/jorge/Downloads/${targetSongNamesPrefix}.mp3`;
 const timeTable = '/home/jorge/Personal/cutmp3/timetable.txt';
 const targetDirPath = '/home/jorge/Personal/cutmp3/splitted-songs'
-const targetSongNamesPrefix = '20230502_';
 
 function durationToSeconds(start) {
     return moment.duration(start).asSeconds();
 }
 
 const songs = fs.readFileSync(timeTable, 'utf-8').split('\n');
+let songNumber = 0;
 for (const song of songs) {
-    const [start, end, name] = song.trim().split(',');
+    let [start, end, name] = song.trim().split(',');
 
     const secondsStart = durationToSeconds(start);
     const secondsEnd = durationToSeconds(end);
     const duration = secondsEnd - secondsStart;
     if (!name) {
-        break;
+        name = 'Untitled';
     }
 
     console.log(`Start processing ${name}...`);
-    const outputPath = `${targetDirPath}/${targetSongNamesPrefix}${name}.mp3`;
+    const outputPath = `${targetDirPath}/${targetSongNamesPrefix}_${++songNumber}-${name}.mp3`;
     ffmpeg(sourceAudioPath)
         .seekInput(secondsStart)
         .duration(duration)
